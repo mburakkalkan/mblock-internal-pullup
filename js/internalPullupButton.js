4e11fabdc3cac6c51cc1001fed8a14c47fb9b86a
M. Burak Kalkan <mburakkalkan@gmail.com>
2019-03-29 09:48:47 +0300

    Add files via upload

---
+++ js/internalPullupButton.js
@@ -0,0 +1,62 @@
+// input-pullup.js
+
+(function(ext) {
+    var device = null;
+    
+    
+	ext.internalPullupButton = function(){
+		
+	};
+
+    function processData(bytes) {
+        trace(bytes);
+    }
+
+    // Extension API interactions
+    var potentialDevices = [];
+    ext._deviceConnected = function(dev) {
+        potentialDevices.push(dev);
+
+        if (!device) {
+            tryNextDevice();
+        }
+    }
+
+    function tryNextDevice() {
+        // If potentialDevices is empty, device will be undefined.
+        // That will get us back here next time a device is connected.
+        device = potentialDevices.shift();
+        if (device) {
+            device.open({ stopBits: 0, bitRate: 115200, ctsFlowControl: 0 }, deviceOpened);
+        }
+    }
+
+    function deviceOpened(dev) {
+        if (!dev) {
+            // Opening the port failed.
+            tryNextDevice();
+            return;
+        }
+        device.set_receive_handler('internalPullupButton',function(data) {
+            processData(data);
+        });
+    };
+
+    ext._deviceRemoved = function(dev) {
+        if(device != dev) return;
+        device = null;
+    };
+
+    ext._shutdown = function() {
+        if(device) device.close();
+        device = null;
+    };
+
+    ext._getStatus = function() {
+        if(!device) return {status: 1, msg: 'internalPullupButton disconnected'};
+        return {status: 2, msg: 'internalPullupButton connected'};
+    }
+
+    var descriptor = {};
+	ScratchExtensions.register('internalPullupButton', descriptor, ext, {type: 'serial'});
+})({});
